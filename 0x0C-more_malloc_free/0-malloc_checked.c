9cb264eee0a0d80b23c7fa98812e5afcea6938a8
koshmane <olayinkaoadedeji@gmail.com>
2023-04-13 13:11:14 -0700

    function that allocates memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -19,20 +19,3 @@ void *malloc_checked(unsigned int b)
 
 	return (p);
 }
- 1  
-0x0C-more_malloc_free/README.md
-@@ -0,0 +1 @@
-0x0C. C - More malloc, free
- 10  
-0x0C-more_malloc_free/main.h
-@@ -0,0 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-#endif
