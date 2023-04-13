c4a10041ae90c155b4e1812bb034fe14847d9df8
koshmane <olayinkaoadedeji@gmail.com>
2023-04-13 13:07:36 -0700

    function that allocates memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,30 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
-  * create_array - creates an array of chars
-  * @size: The size of the array
-  * @c: The char to fill in the array
+  * malloc_checked - Allocates memory
+  * @b: the size to allocate
   *
-  * Return: The array filled
+  * Return: Nothing.
   */
-char *create_array(unsigned int size, char c)
+void *malloc_checked(unsigned int b)
 {
-	unsigned int i;
-	char *s;
+	void *p;
 
-	if (size == 0)
-		return (NULL);
+	p = malloc(b);
 
-	s = malloc(size * sizeof(char));
+	if (p == NULL)
+		exit(98);
 
-	if (s == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-	{
-		s[i] = c;
-	}
-
-	return (s);
+	return (p);
 }
+ 1  
+0x0C-more_malloc_free/README.md
+@@ -0,0 +1 @@
+0x0C. C - More malloc, free
+ 10  
+0x0C-more_malloc_free/main.h
+@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
