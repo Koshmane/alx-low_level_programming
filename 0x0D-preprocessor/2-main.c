97febb218b3b30b3cb40699f2d46e5d6c23b0355
koshmane <olayinkaoadedeji@gmail.com>
2023-04-17 13:45:41 -0700

    Macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int ain(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
+Main - prints the name of the file
+
+Return: Always 0
+Int main(void)
+
+Printf(“%s\n”, _File_);
+Return (0);
